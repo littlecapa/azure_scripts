8980d37f52ef5654a84b26eadf3800d05a5d6cf1
Roger Lorenz <littlecapa@Air-von-Roger.fritz.box>
2022-07-09 15:12:44 +0200

    Changed Server Type

--- create_postgres/create_postgres.sh
+++ create_postgres/create_postgres.sh
@@ -20,24 +20,40 @@ echo "Creating $resourceGroup in $location..."
 az group create --name $resourceGroup --location "$location" --tags $tag
 
 server="postgresql-server-roger"
-sku="GP_Gen5_2"
+sku="Standard_B1ms"
+tier="Burstable"
+storage="32"
 login="azureuser"
 # Specify appropriate IP address values for your environment
 # to limit / allow access to the PostgreSQL server
-startIp=79.0.0.0
-endIp=80.0.0.0
+startIp=0.0.0.0
+endIp=255.255.255.255
 echo "Using resource group $resourceGroup with login: $login, password: $password..."
 
 # Create a PostgreSQL server in the resource group
 # Name of a server maps to DNS name and is thus required to be globally unique in Azure.
 echo "Creating $server in $location..."
-az postgres server create --name $server --resource-group $resourceGroup --location "$location" --admin-user $login --admin-password $password --sku-name $sku
+az postgres flexible-server create \
+    --name $server \
+    --resource-group $resourceGroup \
+    --location "$location" \
+    --admin-user $login \
+    --admin-password $password \
+    --sku-name $sku \
+    --tier $tier \
+    --storage-size $storage \
+    --yes
 
 # Configure a firewall rule for the server 
 echo "Configuring a firewall rule for $server for the IP address range of $startIp to $endIp"
-az postgres server firewall-rule create --resource-group $resourceGroup --server $server --name AllowIps --start-ip-address $startIp --end-ip-address $endIp
+az postgres server firewall-rule create \
+    --resource-group $resourceGroup \
+    --server $server \
+    --name AllowIps \
+    --start-ip-address $startIp \
+    --end-ip-address $endIp
 
-az postgres server show --resource-group $resourceGroup --name $server
-
-#psql --host=<server_name>.postgres.database.azure.com --port=5432 --username=<admin_user>@<server_name> --dbname=postgres
+az postgres server show \
+    --resource-group $resourceGroup \
+    --name $server
 
